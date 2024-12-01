20875535a37e5cb4a90fb69af87fbdff69e91dbe
offici5l <offici5l@protonmail.com>
2024-12-01 16:13:29 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+### Firmware-Content-Extractor  
+**Extract files from firmware without downloading the full firmware**
+
+- **option**: `boot_img` or `settings_apk` or `init_boot_img`
+
+- **url**: The URL to the `.zip` firmware file.
+
+___
+
+#### **Usage:**
+
+**Method 1. Using `curl`:**
+
+```bash
+curl "fce.offici5l.workers.dev?get=<option>&url=<url>"
+```
+
+**Method 2. Using a `browser`:**
+
+Open the following link in your browser:
+
+`fce.offici5l.workers.dev?get=<option>&url=<url>`
---
+++ cloudflare/index.js
@@ -0,0 +1,80 @@
+export default {
+  async fetch(req, env) {
+    const urlParams = new URLSearchParams(req.url.split('?')[1]);
+    const get = urlParams.get('get');
+    const url = urlParams.get('url');
+
+    if (!get || !url) {
+      return new Response("\nMissing parameters!\n\nUsage: \ncurl fce.offici5l.workers.dev?get=<boot_img|settings_apk|init_boot_img>&url=<url>\n\nExample:\n curl fce.offici5l.workers.dev?get=boot_img&url=https://example.com/file.zip\n\n", { status: 400 });
+    }
+
+    if (get !== "boot_img" && get !== "settings_apk" && get !== "init_boot_img") {
+      return new Response("\nOnly 'boot_img', 'settings_apk', and 'init_boot_img' are allowed for 'get' parameter.\n", { status: 400 });
+    }
+
+    if (!url.endsWith(".zip")) {
+      return new Response("\nOnly .zip URLs are supported.\n", { status: 400 });
+    }
+
+    const response = await fetch(url, { method: 'HEAD' });
+    if (!response.ok) {
+      return new Response("\nThe provided URL is not accessible.\n", { status: 400 });
+    }
+
+    const fileName = url.split('/').pop();
+    const combinedBasename = `${get}_${fileName}`;
+    const finalUrl = `https://github.com/offici5l/Firmware-Content-Extractor/releases/download/${get}/${combinedBasename}`;
+
+    const headers = {
+      "Authorization": `token ${env.GTKK}`,
+      "Accept": "application/vnd.github.v3+json",
+      "Content-Type": "application/json",
+      "User-Agent": "Cloudflare Worker"
+    };
+
+    const BaseUrl = "https://api.github.com/repos/offici5l/Firmware-Content-Extractor/actions/workflows/FCE.yml";
+
+    const githubDispatchUrl = `${BaseUrl}/dispatches`;
+    const TRACK_URL = `${BaseUrl}/runs`;
+
+    try {
+      const finalUrlResponse = await fetch(finalUrl, { method: 'HEAD' });
+      if (finalUrlResponse.ok) {
+        return new Response(`\nresult: available\nlink: ${finalUrl}\n`, { status: 200 });
+      }
+
+      const track = Date.now().toString();
+      const data = { ref: "main", inputs: { get, url, track } };
+
+      const githubResponse = await fetch(githubDispatchUrl, {
+        method: "POST",
+        headers: headers,
+        body: JSON.stringify(data)
+      });
+
+      if (githubResponse.ok) {
+        while (true) {
+          const trackResponse = await fetch(TRACK_URL, { method: "GET", headers });
+          if (trackResponse.ok) {
+            const workflowRuns = await trackResponse.json();
+            for (const jobUrl of workflowRuns.workflow_runs.map(run => run.url + "/jobs")) {
+              const jobResponse = await fetch(jobUrl, { method: "GET", headers });
+              if (jobResponse.ok) {
+                const jobData = await jobResponse.json();
+                const job = jobData.jobs.find(job => job.name === track);
+                if (job) {
+                  return new Response(`\n\nIt will be available at this link: ${finalUrl}\n\nTrack progress: ${job.html_url}\n\n`, { status: 200 });
+                }
+              }
+            }
+          }
+        }
+      } else {
+        const githubResponseText = await githubResponse.text();
+        return new Response(`GitHub Response Error: ${githubResponseText}`, { status: 500 });
+      }
+    } catch (error) {
+      return new Response(`Error: ${error.message}`, { status: 500 });
+    }
+  }
+};
